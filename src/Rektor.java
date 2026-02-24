d26b05cb649e42e6d11b805f48052ce4e52274c0
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2026-02-24 15:11:46 +0100

    Klar med rektor

--- src/Rektor.java
+++ src/Rektor.java
@@ -1,3 +1,34 @@
-public class Rektor extends skolPersoner{
-    
+public class Rektor extends skolPersoner {
+    protected String arbetsrum;
+
+    // Konstruktor
+    public Rektor(String namn, int ålder, String personnummer, String arbetsrum) {
+        super(namn, ålder, personnummer);
+
+        if (arbetsrum == null || arbetsrum.trim().isBlank()) {
+            throw new IllegalArgumentException("Arbetsrum får inte vara tomt");
+        } else {
+            this.arbetsrum = arbetsrum;
+        }
+    }
+
+    // Getters
+    public String getArbetsrum() {
+        return arbetsrum;
+    }
+
+    // Setters
+    public void setArbetsrum(String arbetsrum){
+        if (arbetsrum == null || arbetsrum.trim().isBlank()) {
+            throw new IllegalArgumentException("Arbetsrum får inte vara tomt");
+        } else{
+            this.arbetsrum=arbetsrum;
+        }
+    }
+
+    // toString
+    @Override
+    public String toString(){
+        return "Retor - " + super.toString() + ", Arbetsrum: " + arbetsrum ".";
+    }
 }
