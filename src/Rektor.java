40fe8d0e89c1ca669c63294f782cdf9a32d33812
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2026-02-24 14:56:07 +0100

    Klar med skolPersoner klassen

---
+++ src/Rektor.java
@@ -0,0 +1,3 @@
+public class Rektor extends skolPersoner{
+    
+}
--- src/skolPersoner.java
+++ src/skolPersoner.java
@@ -1,3 +1,59 @@
 public class skolPersoner {
+    protected String namn;
+    protected int ålder;
+    protected String personnummer;
     
+
+    // Konstruktor
+    public skolPersoner(String namn, int ålder, String personnummer){
+        if (namn == null || namn.trim().isBlank()) {throw new IllegalArgumentException("Namnet får inte vara tom");}
+        else if (ålder < 0) {throw new IllegalArgumentException("Ålder får inte vara negativt");}
+        else if (personnummer == null ||  personnummer.trim().isBlank()) {throw new IllegalArgumentException("Personnummret får inte vara tomt");}
+        else{
+            this.namn=namn;
+            this.ålder=ålder;
+            this.personnummer=personnummer;
+        }
+    }
+
+    // Getters
+    public String getNamn(){
+        return namn;
+    }
+    
+    public int getÅlder(){
+        return ålder;
+    }
+
+    public String getPersonnummer(){
+        return personnummer;
+    }
+
+    // Setters
+    public void setNamn(String namn){
+        if (namn == null || namn.trim().isBlank()) {
+            throw new IllegalArgumentException("Namnet får inte vara tomt");
+        }
+        else{this.namn=namn;}
+    }
+
+    public void setÅlder(int ålder){
+        if (ålder < 0) {
+            throw new IllegalArgumentException("Ålder får inte vara negativt");
+        }
+        else{this.ålder=ålder;}
+    }
+
+    public void setPersonnumer(String personnumer){
+        if (personnummer == null ||  personnummer.trim().isBlank()) {
+            throw new IllegalArgumentException("Personnummret får inte vara tomt");
+        }
+        else{this.personnummer=personnumer;}
+    }
+
+    // toString
+    @Override
+    public String toString(){
+        return "Skolperson - Namn: " + namn + ", Ålder: " + ålder + ", Personnummer: " + personnummer + ".";
+    }
 }
